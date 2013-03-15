78e79fe43f86473da9d809eddd974530b30e9dfa
Brandon-Lyons <brandon.joseph.lyons@gmail.com>
2013-03-15 13:02:59 -0500

    twitter feed on the side, ongoing content in about, animations

--- scripts/main.js
+++ scripts/main.js
@@ -4,19 +4,23 @@ $(document).ready(function(){
 
   $('#first_section_link').click(function(e) {
     $('.main-content').hide();
-    $('#first-content-div').show();
+    $('.title-description').slideUp(1000);
+    $('#first-content-div').slideDown(1000);
     e.preventDefault();
   });
 
   $('#second_section_link').click(function(e) {
     $('.main-content').hide();
-    $('#second-content-div').show();
+    $('.title-description').slideUp(1000);
+    $('#second-content-div').slideDown(1000);
     e.preventDefault();
   });
 
   $('#third_section_link').click(function(e) {
     $('.main-content').hide();
-    $('#third-content-div').show();
+    $('.title-description').slideUp(1000);
+    $('#third-content-div').slideDown(1000);
+   setTimeout(getMap, 1000);
     e.preventDefault();
   });
 
@@ -26,7 +30,7 @@ $(document).ready(function(){
     $.ajax({
       dataType:"jsonp", 
       type:"GET", 
-      url:"http://search.twitter.com/search.json?q=psoriasis",
+      url:"https://search.twitter.com/search.json?q=%23psoriasis",
       success:function(response) {
         tweets = (response.results);
         $.each(tweets, function() { $("#tweets").append("<li><h5>" + this.text + "</h5></li>") });
@@ -36,63 +40,66 @@ $(document).ready(function(){
   };
 
 
-  $('#treatTabs a').tab();
-  /*
+  $('#myTab a').tab();
+  
+ var getMap = function() {
   var mapOptions = {
     center: new google.maps.LatLng(36.1666667, -86.78333329999998),
     zoom: 15,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    mapTypeId: google.maps.MapTypeId.SATELLITE
   };
-  var map = new google.maps.Map(document.getElementById("map"),
-                                mapOptions);
 
-                                var autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocomplete'));
-                                autocomplete.bindTo('bounds',map);
+  var map = new google.maps.Map(document.getElementById("map"),mapOptions);
 
-                                var service = new google.maps.places.PlacesService(map);
+  var autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocomplete'));
+  autocomplete.bindTo('bounds',map);
 
-                                google.maps.event.addListener(autocomplete, 'place_changed', function() {
-                                  var infoWindow = new google.maps.InfoWindow();
-                                  infoWindow.close();
-                                  var loc = autocomplete.getPlace();
-                                  var lat= loc.geometry.location.mb;
-                                  var lon= loc.geometry.location.nb;
-                                  var request= {
-                                    location: new google.maps.LatLng (lat, lon),
-                                    radius: '5000',
-                                    query: 'dermatologist'
-                                  };
-                                  service.textSearch(request, callback);
-                                  if (loc.geometry.viewport) {
-                                    map.fitBounds(loc.geometry.viewport);
-                                  } else {
-                                    map.setCenter(loc.geometry.location);
-                                    map.setZoom(15);
-                                  }
+  var service = new google.maps.places.PlacesService(map);
 
-                                });  //en google.maps event listener
-                                function callback(results, status) {
-                                  if (status===google.maps.places.PlacesServiceStatus.OK) {
-                                    for (var i=0; i<results.length; i++){
-                                      var place= results[i];
-                                      createMarker(results[i]);
-                                    };
-                                  };
-                                  console.log(results.length);
-                                };
-                                function createMarker (place) {
-                                  var marker = new google.maps.Marker({
-                                    map: map
-                                  });
-                                  var infoWindow = new google.maps.InfoWindow();
-                                  marker.setPosition(place.geometry.location);
-                                  infoWindow.setContent('<div><strong>' + place.name + '</strong><br><p>'+ place.formatted_address +'</p><a href="'+ place.website +'" >'+ place.website +'</a>');
-                                  google.maps.event.addListener(marker,'click',function(e){
-                                    infoWindow.open(map, marker);
-                                  });
-                                  google.maps.event.addListener(map, 'click', function(e){
-                                    infoWindow.close();
-                                  })
-                                };
-                                */
+  google.maps.event.addListener(autocomplete, 'place_changed', function() {
+    var infoWindow = new google.maps.InfoWindow();
+    infoWindow.close();
+    var loc = autocomplete.getPlace();
+    var lat= loc.geometry.location.mb;
+    var lon= loc.geometry.location.nb;
+    var request= {
+      location: new google.maps.LatLng (lat, lon),
+      radius: '5000',
+      query: 'dermatologist'
+    };
+    service.textSearch(request, callback);
+    if (loc.geometry.viewport) {
+      map.fitBounds(loc.geometry.viewport);
+    } else {
+      map.setCenter(loc.geometry.location);
+      map.setZoom(15);
+    }
+ });  //en google.maps event listener
+
+  function callback(results, status) {
+    if (status===google.maps.places.PlacesServiceStatus.OK) {
+      for (var i=0; i< results.length; i++){
+        var place= results[i];
+        createMarker(results[i]);
+      };
+    };
+    console.log(results);
+  };
+
+  function createMarker (place) {
+    var marker = new google.maps.Marker({
+      map: map
+    });
+
+    var infoWindow = new google.maps.InfoWindow();
+    marker.setPosition(place.geometry.location);
+    infoWindow.setContent('<div><strong>' + place.name + '</strong><br><p>'+ place.formatted_address +'</p><a href="'+ place.website +'" >'+ place.website +'</a>');
+    google.maps.event.addListener(marker,'click',function(e){
+      infoWindow.open(map, marker);
+    });
+    google.maps.event.addListener(map, 'click', function(e){
+      infoWindow.close();
+    })
+  };
+       }   ;                      
 });  //end ready
