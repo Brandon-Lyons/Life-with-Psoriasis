b9e3d77e7ddc9d4ac0d75601b791030a1d676c99
Brandon-Lyons <brandon.joseph.lyons@gmail.com>
2013-03-27 04:07:58 -0500

    fixed map infoWindows and style tweaks

--- scripts/main.js
+++ scripts/main.js
@@ -15,7 +15,7 @@ function deletePost(id) {
 
 
 //**************initial forum loading on refresh*******************************
- function loadForum() {
+function loadForum() {
   $.ajax({
     dataType:"json",
     type:"GET",
@@ -42,8 +42,8 @@ function addPost(content, name) {
     type:"POST",
     url:"backliftapp/posts",
     data: {text: content,
-           name: name
-          },
+      name: name
+    },
     success:function(response) {
       refreshPosts();
       $('#new_post').val("");
@@ -78,12 +78,15 @@ $(document).ready(function(){
   $('.section-link').click(function(e){
     $('.main-content').hide();
     $('.title-description').slideUp(1000);
-    $($(this).attr('data-link')).slideDown(1000);
+    if (this.id == "third_section_link") {
+      $($(this).attr('data-link')).slideDown(1000, getMap);
+    }
+    else {
+      $($(this).attr('data-link')).slideDown(1000);
+    }
     e.preventDefault();
-    setTimeout(getMap, 1000);
     refreshPosts();
   });
-
 
   $('.treatment-div a').click(function(){
     $(this).next('.treatment').slideToggle();
@@ -95,38 +98,47 @@ $(document).ready(function(){
 
   getTweets();
 
-//*************Forum validation function***********************
+  //*************Forum validation function***********************
   $('#submit_post').click(function() {
     if ($('#new_post').val()=="" || $('#post_name').val()==""){
       alert('please fill all inputs.');
       return false
     }
     else{
-    addPost($('#new_post').val(), $('#post_name').val());
-  }
+      addPost($('#new_post').val(), $('#post_name').val());
+    }
   });
 
-
- 
   $('#myTab a').tab();  //bootstrap tab function
-  
+
   //**************************setup funtions for Google Maps*********************************
   var getMap = function() {
-  var mapOptions = {
-    center: new google.maps.LatLng(36.1666667, -86.78333329999998),
-    zoom: 15,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
+    initializeMap();
+    var map;
+    var infowindow;
+  }
 
-    var map = new google.maps.Map(document.getElementById("map"),mapOptions);
-
+  function initializeMap() {
+    var nashville = new google.maps.LatLng(36.167192, -86.787113);
     var autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocomplete'));
 
+    map = new google.maps.Map(document.getElementById('map'), {
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      center: nashville,
+      zoom: 15
+    });
+
+    var request= {
+      location: nashville,
+      radius: '5000',
+      keyword: 'dermatologist'
+    };
+
+    infowindow = new google.maps.InfoWindow();
     var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
 
     google.maps.event.addListener(autocomplete, 'place_changed', function() {
-      var infoWindow = new google.maps.InfoWindow();
-      infoWindow.close();
       var loc = autocomplete.getPlace();
       var lat= loc.geometry.location.kb;
       var lon= loc.geometry.location.lb;
@@ -142,51 +154,45 @@ $(document).ready(function(){
         map.setCenter(loc.geometry.location);
         map.setZoom(15);
       }
-   });  //end google.maps event listener
-    function callback(results, status) {
-      if (status===google.maps.places.PlacesServiceStatus.OK) {
-        for (var i=0; i< results.length; i++){
-          var place= results[i];
-          createMarker(results[i]);
-        };
-      };
-    };
+    });  //en google.maps event listener
+  }
 
-  //********************creates markers and infowindows at search result locations********************
-    function createMarker (place) {
-      var marker = new google.maps.Marker({
-        map: map
-      });
+  function callback(results, status) {
+    google.maps.event.trigger(map, 'resize');
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        createMarker(results[i]);
+      }
+    }
+  }
 
-      var infoWindow = new google.maps.InfoWindow();
-      marker.setPosition(place.geometry.location);
-      infoWindow.setContent('<strong>' + place.name + '</strong><br><p>'+ place.vicinity +'</p>');
-      google.maps.event.addListener(marker, 'click', function() {
-        if ($(infoWindow).attr('chosen')){
-          infoWindow.close();
-        }
-        marker.setZIndex(500, this);
-        infoWindow.open(map, this);
-        $(infoWindow).attr('chosen', 'true')
-        infoWindow.setZIndex(500, this);
-      });
-    };
-    };
+  function createMarker(place) {
+    var marker = new google.maps.Marker({
+      map: map,
+      position: place.geometry.location
+    });
+    marker.setAnimation(google.maps.Animation.DROP);
+
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.setContent('<strong>' + place.name + '</strong>' + '<br />' + place.vicinity);
+      infowindow.open(map, this);
+    });
+  }
 
   //********************uses Google Feeds to display RSS feed**************************************
-    function loadNewsFeed() {
-      var feed = new google.feeds.Feed("http://www.medicalnewstoday.com/rss/eczema-psoriasis.xml");
-      feed.includeHistoricalEntries();  //includes any past entries it finds
-      feed.setNumEntries(5);  //sets entries limit at 5
-      feed.load(function(result){
-        if (!result.error) { 
-          var container = $('#newsFeedInner');
-          for (var i=0; i< result.feed.entries.length; i++) {
-            var entry = result.feed.entries[i];
-            $(container).append("<p><a href='" + entry.link + "'>" + entry.title + "</a><br></p>"); //if there is no error return entry url and title
-          };
-        }
-      })
-    } ;
-    google.setOnLoadCallback(loadNewsFeed);
+  function loadNewsFeed() {
+    var feed = new google.feeds.Feed("http://www.medicalnewstoday.com/rss/eczema-psoriasis.xml");
+    feed.includeHistoricalEntries();  //includes any past entries it finds
+    feed.setNumEntries(5);  //sets entries limit at 5
+    feed.load(function(result){
+      if (!result.error) {
+        var container = $('#newsFeedInner');
+        for (var i=0; i< result.feed.entries.length; i++) {
+          var entry = result.feed.entries[i];
+          $(container).append("<p><a href='" + entry.link + "'>" + entry.title + "</a><br></p>"); //if there is no error return entry url and title
+        };
+      }
+    })
+  } ;
+  google.setOnLoadCallback(loadNewsFeed);
 });  //end ready
